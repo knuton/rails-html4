40533f32bc387c362c136b14017236baad287efa
Johannes Emerich <johannes@emerich.de>
2009-11-25 10:15:13 +0100

    Stay DRY

--- init.rb
+++ init.rb
@@ -1,13 +1,14 @@
 module ::ActionView::Helpers::TagHelper
   alias :xhtml_tag :tag
-
-  def tag(name, options = nil, open = false, escape = true)
-    "<#{name}#{tag_options(options, escape) if options}>"
+  
+  # Why not just use the original function?
+  def tag(name, options = nil, open = true, escape = true)
+    xhtml_tag(name, options, open, escape)
   end
 end
 
 ActionView::Helpers::InstanceTag.class_eval do
-  def tag_without_error_wrapping(name, options = nil, open = false, escape = true)
+  def tag_without_error_wrapping(name, options = nil, open = true, escape = true)
     "<#{name}#{tag_options(options, escape) if options}>"
   end
 end
